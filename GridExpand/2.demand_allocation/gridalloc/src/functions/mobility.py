e12018b7c0de19d7b7cbba2b5fc59a438490938e
pedrocaixeta <caixeta_pedro@yahoo.com.br>
2026-07-02 14:36:30 +0200

    correct range in for loop for trying attemps

--- GridExpand/2.demand_allocation/gridalloc/src/functions/mobility.py
+++ GridExpand/2.demand_allocation/gridalloc/src/functions/mobility.py
@@ -255,7 +255,7 @@ def get_mobility_demand(vehicles, weather):
         warnings.simplefilter("ignore", FutureWarning)
         # all_timeseries, all_batteries = _simulate_vehicles(vehicles, weather)
         max_retries = 3
-        for attempt in range(1, max_retries):
+        for attempt in range(1, max_retries+1):
             try:
                 all_timeseries, all_batteries = _simulate_vehicles(vehicles, weather)
                 break
