3cd4030557b0efe4ee6598d8b43ee78afc916902
pedrocaixeta <caixeta_pedro@yahoo.com.br>
2026-07-09 20:10:29 +0200

    resolve process hanging at shutdown

--- GridExpand/4.powerflow/run_cluster_serialstd.sh
+++ GridExpand/4.powerflow/run_cluster_serialstd.sh
@@ -25,12 +25,23 @@ echo "Script started at: $start_time"
 INDEX="$1"
 echo "Fileindex: $INDEX"
 
+# Set thread variables to prevent nested threading deadlocks in multiprocessing
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export VECLIB_MAXIMUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+
 srun python3 run_pwrflw.py $INDEX --n_cpu $SLURM_CPUS_PER_TASK
 wait
 
-### Delete error log file at end of run if it is empty
+### Delete error log file at end of run if it is empty, otherwise append the file index
 ERR_FILE="logs/errors/${SLURM_JOB_ID}_error.log"
 
-if [ -f "$ERR_FILE" ] && [ ! -s "$ERR_FILE" ]; then
-  rm "$ERR_FILE"
+if [ -f "$ERR_FILE" ]; then
+  if [ ! -s "$ERR_FILE" ]; then
+    rm "$ERR_FILE"
+  else
+    echo "Fileindex: $INDEX" >> "$ERR_FILE"
+  fi
 fi
