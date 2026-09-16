bb20050bb68534a73508cd212474ba0686af2af9
Pedro Caixeta <pedro.caixeta@tum.de>
2026-09-16 17:46:03 +0200

    Zero-pad input ID to match 4-digit prefixes in 4.powerflow

--- GridExpand/4.powerflow/run_pwrflw.py
+++ GridExpand/4.powerflow/run_pwrflw.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     all_entries = os.listdir(config.DATA_DIR)
     h5_files = [fname for fname in all_entries if fname.endswith(".h5")]
     # find file with correct id prefix
-    input_id_str = str(args.inputfile_id)
+    input_id_str = str(args.inputfile_id).zfill(4)
     matched_files = [fname for fname in h5_files if fname.split('_', 1)[0] == input_id_str]
     input_file = matched_files[0]
 
