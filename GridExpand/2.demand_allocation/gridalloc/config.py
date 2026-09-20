e72ce4e5a08404c11b4dacb544c5ec5b268ba790
Pedro Caixeta <pedro.caixeta@tum.de>
2026-09-20 13:43:18 +0200

    Enable run of 2.demand_allocation on Ellias grids - To verify if the changes applied on 2.Demand_allocation don't mess up the intended/original logic

--- GridExpand/2.demand_allocation/gridalloc/config.py
+++ GridExpand/2.demand_allocation/gridalloc/config.py
@@ -6,9 +6,9 @@ class Config:
     #----------------- Paths/Dataset Readout ----------------------#
     #--------------------------------------------------------------#
     ##### Important data paths #####
-    DATA_GRID_DIR = "/dss/dssfs05/lwp-dss-0003/pn98cu/pn98cu-dss-0001/PedroC/3rd_RUN/1.Grid_Sampled/"           # Directory from which to read the pylovo grid input data
+    DATA_GRID_DIR = "/dss/dssfs05/lwp-dss-0003/pn98cu/pn98cu-dss-0001/EliasH/RawGrids/"           # Directory from which to read the pylovo grid input data
     DATA_STAT_DIR = "data/statistics"      # Directory from which to read data for computing demands
-    STORAGE_DIR = "/dss/dssfs05/lwp-dss-0003/pn98cu/pn98cu-dss-0001/PedroC/3rd_RUN/2.Demand_Allocated/"                # Directory in which to store resulting urbs input files
+    STORAGE_DIR = "/dss/dssfs05/lwp-dss-0003/pn98cu/pn98cu-dss-0001/PedroC/3rd_RUN/Verify 2.Dmd_alloc on Elias's Grids/"                # Directory in which to store resulting urbs input files
 
     #--------------------------------------------------------------#
     #-------------- Weather Data API Connections ------------------#
